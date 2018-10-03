9935dbdf1bd7d4fa14703411ed8cc1e9608af4ea
Hector Martinez <43742306+hecmtz@users.noreply.github.com>
2018-10-03 11:49:19 +0200

    Comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 
+             int i = 1;
+            while (i < 101)
+            {
+                Console.WriteLine("Numero " + i);
+                i++;
+            }
+
+            Console.ReadLine();
+            */
+            /*
+            while (i > 0)
+            {
+                Console.WriteLine("Inserta un numero");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //do
+            //{
+            //    Console.WriteLine("Inserta un numero");
+            //    i = Convert.ToInt32(Console.ReadLine());
+            //} while (i<0);
+            
+             */
+            int num , num1=0 ;
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Inserta el valor del numero "+ i );
+                num = Convert.ToInt32(Console.ReadLine());
+                num1 = num1 + num; // se suman el valor antiguo y el nuevo
+                
+
+            }
+            Console.WriteLine("La suma es: " + num1);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
